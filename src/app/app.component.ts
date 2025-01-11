dd14cc64e929ce00621cbd2969d746a2350c02b6
Timur Popov <tim487665@gmail.com>
2025-01-11 22:09:58 +0300

    Controls component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdvertComponent } from './advert/component';
+import { ControlsComponent } from './controls/component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    AdvertComponent
+    AdvertComponent, ControlsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
---
+++ src/app/controls/component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controls',
+  imports: [],
+  templateUrl: './template.html',
+  styleUrl: './styles.less'
+})
+export class ControlsComponent {
+
+}
