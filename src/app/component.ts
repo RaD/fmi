aa7491bc2b341a2d49b8b00d14d5aa016bf89439
Ruslan Popov <ruslan.popov@gmail.com>
2025-07-28 10:17:46 +0300

    Router outlet usage

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WelcomeComponent } from '../pages/welcome/component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    WelcomeComponent,
   ],
   templateUrl: './template.html',
   styleUrl: './styles.less'
